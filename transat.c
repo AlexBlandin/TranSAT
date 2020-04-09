6dab6753f3b708f3b540e1777a0efd872b177152
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-04-09 11:05:54 +0100

    Keep it from complaining about not using solutions

--- transat.c
+++ transat.c
@@ -28,15 +28,17 @@ static u64 solutions[] = {1, 1, 0, 0, 2, 10, 4, 40, 92, 352,
 
 int main() {
   seed_rng();
-  u64 *sb = NULL; // stretchy buffer
-  u64 my_int = 10;
-  sb_push(sb, my_int);
+  u8 *sb = NULL; // stretchy buffer
+  u8 d6 = (u8)(randint()%6);
+  sb_push(sb, d6);
   sb_push(sb, 3);
   sb_push(sb, 0);
-  sb_push(sb, randint()%100);
+  sb_push(sb, (u8)(randint()%100));
   sb_count(sb); // == 1
   sb_last(sb);  // lvalue of last item, == 42
-  printf("Hello world! Say it again %ld times louder!\n", sb[3]);
+  printf("Hello world! Say it again %d times louder!\n", sb[3]);
+  u8 sol = randint()%28;
+  printf("Q(%d x %d) is %ld.\n", sol, sol, solutions[sol]);
   sb_free(sb);
   return 0;
 }
