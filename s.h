ffd20638ceb01faa0d3090c95fdb017fceef2f1a
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-04-09 10:39:22 +0100

    Typedef u64 etc shorthands for use in anonymous structs/unions

--- s.h
+++ s.h
@@ -3,26 +3,26 @@
 #define SHORTHAND_H_INCLUDED
 
 #include <assert.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <time.h>
 
-#define u64 uint64_t
-#define u32 uint32_t
-#define u16 uint16_t
-#define u8 uint8_t
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef uint8_t u8;
 
 #define u64_MAX UINT64_MAX
 #define u32_MAX UINT32_MAX
 #define u16_MAX UINT16_MAX
-#define u8_MAX  UINT8_MAX
+#define u8_MAX UINT8_MAX
 
-#define i64 int64_t
-#define i32 int32_t
-#define i16 int16_t
-#define i8 int8_t
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef int8_t i8;
 
 #define i64_MAX INT64_MAX
 #define i32_MAX INT32_MAX
@@ -49,7 +49,7 @@
 
 #define not !
 #define and &&
-#define or  ||
+#define or ||
 
 #ifndef true
 #define true 1
