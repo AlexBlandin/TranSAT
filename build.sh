20e5e1a9bf716c3be5bbbfe92c1cf9ca929b13f1
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-04-21 14:09:48 +0100

    Put time in the build

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-clear && musl-gcc -fplan9-extensions -Wall -Wextra -Wno-unused-value -Wno-unused-variable -Wimplicit-fallthrough=0 -static -Ofast -march=native $2 $1.c -o $1 && ./$1
+clear && musl-gcc -fplan9-extensions -Wall -Wextra -Wno-unused-value -Wno-unused-variable -Wimplicit-fallthrough=0 -static -Ofast -march=native $2 $1.c -o $1 && time ./$1
