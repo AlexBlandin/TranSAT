b6b47c973c05cbf41c9ee11b47529697a9b1f671
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-04-25 17:44:49 +0100

    After a scare, I have reprogrammed it back to the last working state because git only helps if you actually remember to press enter and actually commit the commit

--- transat.c
+++ transat.c
@@ -11,20 +11,11 @@
 
 /* pick a space any (open) space */
 static inline Slot heuristic() {
-  #if defined(FIRSTROW)
+  #if defined(FIRSTROW) or defined(SQUAREENUM)
   /* from 0,0 indices */
-  for (u8 row = sl.row; row < N; row++)
-    for (u8 col = 0; col < N; col++)
-      if is_open(row, col)
-        return slot(row, col);
-  for (u8 row = 0; row < N; row++)
-    for (u8 col = 0; col < N; col++)
-      if is_open(row, col)
-        return slot(row, col);
-  #elif defined(SQUAREENUM)
-  for (; bd.slot_index < N*N; bd.slot_index++)
-    if is_open(square_enum[bd.slot_index].row, square_enum[bd.slot_index].col)
-      return square_enum[bd.slot_index];
+  for (; bd.i < N*N; bd.i++)
+    if lut_open(bd.i)
+      return lut[bd.i];
   #elif defined(TAW)
   /* TODO: */
   
@@ -34,7 +25,7 @@ static inline Slot heuristic() {
   #else
   #error "You need to choose a heuristic"
   #endif
-  return slot(0, 0);
+  return (Slot) {};
 }
 
 /* is this board solved / trivial to solve now? */
@@ -61,8 +52,6 @@ static inline bool falsified() {
   return rk.open_rows == 0 and rk.open_cols == 0; /* ALO unsatisfiable, as AMO sat is guaranteed by heuristic generation */
 }
 
-/* TODO: speed up Ranks */
-
 /* the TranSAT N-Queens solver */
 static inline void transat() {
   while(board > -1) { /* starts at 0 */
@@ -74,11 +63,11 @@ static inline void transat() {
       pb = false;
 
       /* odd is placed, even is forbid */
-      if (forced or bd.visits & 1) {
+      if (is_queued or bd.visits & 1) {
         /* select branching variable (the slot/space we're focusing on) */
-        if (forced) {
+        if (is_queued) {
           sl = queued;
-          forced = false;
+          is_queued = false;
         } else {
           sl = heuristic(); /* always chooses an open slot, so no need to worry about that */
           if (rk.rows[sl.row].placed or rk.cols[sl.col].placed or rk.dias[sl.dia].placed or rk.adia[sl.adg].placed) {
@@ -93,8 +82,6 @@ static inline void transat() {
         bd.queens_left--;
         placed_sl();
         derank_sl();
-
-        /* TODO: speedup AMO propogation ESPECIALLY THE RANKS */
         
         /* propagate over row and update ranks */
         for (u8 col = 0; rk.rows[sl.row].open and col < sl.col; col++) {
@@ -126,43 +113,30 @@ static inline void transat() {
         }
         clear_full(sl.row, sl.col);
 
-        /* propagate (AMO) over diagonals and update ranks */
-        for (u8 i = 1; i < N; i++) {
-          u8 row1 = sl.row+i; u8 col1 = sl.col+i;
-          u8 row2 = sl.row+i; u8 col2 = sl.col-i;
-          u8 row3 = sl.row-i; u8 col3 = sl.col-i;
-          u8 row4 = sl.row-i; u8 col4 = sl.col+i;
+        /* propagate (AMO) over diagonals and update ranks (top-leftest is 0,0) */
+        for (u8 i = 1; (rk.dias[sl.dia].open or rk.adia[sl.adg].open) and i < N; i++) {
+          u8 up = sl.col-i; u8 down = sl.col+i;
+          u8 left = sl.row-i; u8 right = sl.row+i;
 
-          if (row1 < N and col1 < N) {
-            if is_open(row1, col1) {
-              set(row1, col1);
-              derank(row1, col1);
-              clear_full(row1, col1);
-            }
+          if (left < N and up < N /* and rk.dias[diagonal(left, up)].open */ and is_open(left, up)) {
+            set(left, up);
+            derank(left, up);
+            clear_full(left, up);
           }
-
-          if (row2 < N and col2 < N) {
-            if is_open(row2, col2) {
-              set(row2, col2);
-              derank(row2, col2);
-              clear_full(row2, col2);
-            }
+          if (left < N and down < N /* and rk.adia[antidiagonal(left, down)].open */ and is_open(left, down)) {
+            set(left, down);
+            derank(left, down);
+            clear_full(left, down);
           }
-
-          if (row3 < N and col3 < N) {
-            if is_open(row3, col3) {
-              set(row3, col3);
-              derank(row3, col3);
-              clear_full(row3, col3);
-            }
+          if (right < N and up < N /* and rk.adia[antidiagonal(right, up)].open */ and is_open(right, up)) {
+            set(right, up);
+            derank(right, up);
+            clear_full(right, up);
           }
-
-          if (row4 < N and col4 < N) {
-            if is_open(row4, col4) {
-              set(row4, col4);
-              derank(row4, col4);
-              clear_full(row4, col4);
-            }
+          if (right < N and down < N /* and rk.dias[diagonal(right, down)].open */ and is_open(right, down)) {
+            set(right, down);
+            derank(right, down);
+            clear_full(right, down);
           }
         }
       } else {
@@ -175,17 +149,17 @@ static inline void transat() {
         if (rk.rows[sl.row].open - 1 == 1) { // if, after closing a slot, there is only 1 open, it's a forced move
           for (u8 col = 0; rk.rows[sl.row].open and col < N; col++) {
             if is_open(sl.row, col) {
-              forced = true;
               queued = slot(sl.row, col); // queue a forced move from the same row for the next loop
+              is_queued = true;
               break;
             }
           }
         }
-        if (not forced and rk.cols[sl.col].open - 1 == 1) {
+        if (not is_queued and rk.cols[sl.col].open - 1 == 1) {
           for (u8 row = 0; rk.cols[sl.col].open and row < N; row++) {
             if is_open(row, sl.col) {
-              forced = true;
               queued = slot(row, sl.col); // queue a forced move from the same col for the next loop
+              is_queued = true;
               break;
             }
           }
@@ -197,14 +171,15 @@ static inline void transat() {
 
 int main() {
   init();
-  
-  #ifdef SQUAREENUM
-    for (u16 i = 0; i < N*N; i++) {
+  for (u16 i = 0; i < N*N; i++) {
+  #if defined(FIRSTROW)
+    lut[i] = slot(i/N, i%N);
+  #elif defined(SQUAREENUM)
     u16 r = (u16)ceil(sqrt(i+1));
     u16 d = r*r - i - 1;
-    square_enum[i] = (d < r) ? slot(d, r - 1) : slot(r - 1, 2*r - d - 2);
-  }
+    lut[i] = (d < r) ? slot(d, r - 1) : slot(r - 1, 2*r - d - 2);
   #endif
+  }
 
   transat();
 
--- transat.h
+++ transat.h
@@ -1,14 +1,41 @@
 #ifndef TRANSAT_H_IMPLEMENTED
 #define TRANSAT_H_IMPLEMENTED
 
-/* 22 is the max */
+/* N <= 21 */
 #ifndef N
 #define N 21
 #endif
 
+/* simple bitset for N-Queens row-like structures */
+typedef u32 row_t;
+/* extended bitset for up to 3*N (assuming N <= 21) */
+typedef u64 exrow_t;
+
+const row_t full_row = (1 << N) - 1;
+
+static inline exrow_t embed(row_t row) { return ((exrow_t) row) << N; }
+static inline row_t truncate(exrow_t exrow) { return (row_t) (exrow >> N); }
+static inline exrow_t add(row_t row, exrow_t exrow) { return exrow | (((exrow_t) row) << N); }
+
 #define bd boards[board]
-#define sl boards[board].slot
+#define sl bd.slot
 #define rk boards[board].ranks
+#define index(row, col) (row*N + col)
+#define diagonal(row, col) ((row) + (col))
+#define antidiagonal(row, col) (N - (col) + (row) - 1)
+
+#define at(row, col) (bd.state[(row)] & (1 << (col)))
+#define set(row, col) (bd.state[(row)] |= (1 << (col)))
+#define open(row, col) (bd.state[(row)] &= ~(1 << (col)))
+#define is_open(row, col) (not at(row, col))
+#define lut_open(i) (is_open(lut[i].row, lut[i].col))
+
+typedef struct _Slot {
+  u8 row; /* better to have separated row/col than having to always divide */
+  u8 col;
+  u8 dia; /* diagonal(row, col) */
+  u8 adg; /* antidiagonal(row, col) */
+} Slot;
 
 typedef struct _Rank {
   u8 open: 6; /* how many spaces are open */
@@ -21,52 +48,33 @@ typedef struct _Ranks { /* for the number */
   Rank dias[2*N-1]; /* row + sl */
   Rank adia[2*N-1]; /* N - col + row - 1 */ /* Done this way to handle limited ranges */
 
-  u32 n_open; /* how many are still open at all? */
-
   /* a 1 means that row/col/diagonal is open/forbid/placed */
-  u32 open_rows;   /* which rows are open */
-  u32 open_cols;   /* which cols are open */
-  // u32 placed_rows; /* which rows are placed in */
-  // u32 placed_cols; /* which cols are placed in */
-  // u64 open_dias;   /* which dias are open */
-  // u64 open_adga;   /* which adgs are open */
-  // u64 placed_dias; /* which dias are placed in */
-  // u64 placed_adga; /* which adgs are placed in */
-} Ranks; /* TODO: SPEEDUP SERIOUSLY THIS IS THE SLOWEST THING */
-
-typedef struct _Slot {
-  u8 row; /* better to have separated row/col than having to always divide */
-  u8 col;
-  u8 dia; /* row + col */
-  u8 adg; /* N - col + row - 1 */
-} Slot;
+  row_t open_rows; /* which rows are open */
+  row_t open_cols; /* which cols are open */
+} Ranks;
 
 static inline Slot slot(u8 row, u8 col) {
-  return (Slot) {row, col, row + col, N - col + row - 1};
-} 
+  return (Slot) { row, col, diagonal(row, col), antidiagonal(row, col) };
+}
 
-#define at(row, col) (bd.state[(row)] & (1 << (col)))
-#define set(row, col) (bd.state[(row)] |= (1 << (col)))
-#define open(row, col) (bd.state[(row)] &= ~(1 << (col)))
-#define is_open(row, col) (!at(row, col))
-
+/* ACLS Chessboard */
 typedef struct _Board {
   u8 queens_left; /* how many pieces we have left to place */
+  u16 i; /* where we placed (tyically row*N+col, for lookup in LUT) */
+  Slot slot;
   u16 visits; /* how many times this has been the current board on entry to the main loop */
-  Slot slot; /* where we changed (either forbid or placed) */
-  u16 slot_index; /* to be used if it is needed */
-  u32 state[N]; /* each row on the board as 32bit columns, 0 = open, 1 = forbidden or placed in */
-  Ranks ranks; // taken out since we recompute each time so don't need storage, can speed up later
+  row_t state[N]; /* each row on the board as 32bit columns, 0 = open, 1 = forbidden or placed in */
+  Ranks ranks;
 } Board;
 
 /* DATA */
 static u64 nq = 0; /* solutions */
 static s16 board = 0; /* current board */
 static Board boards[N+1]; /* ALCS boards w/ ranks */
-static Slot square_enum[N*N];
-bool pb = false; /* preempted backtrack */
-bool forced = false;
-Slot queued;
+static Slot lut[N*N]; /* 0..N*N-1 to slot LUT */
+static Slot queued; /* queued index */
+bool pb = false; /* preemptive backtrack to forbid the current slot */
+bool is_queued = false;
 
 static u64 solutions[] = {1, 1, 0, 0, 2, 10, 4, 40, 92, 352,
                           724, 2680, 14200, 73712, 365596,
@@ -75,7 +83,7 @@ static u64 solutions[] = {1, 1, 0, 0, 2, 10, 4, 40, 92, 352,
                           2691008701644, 24233937684440,
                           227514171973736, 2207893435808352,
                           22317699616364044, 234907967154122528};
-                          
+
 /* move along the stack */
 static inline void copy_board() {
   copy(sizeof(Board), boards[board], boards[board+1]);
@@ -86,12 +94,11 @@ static inline void copy_board() {
 /* reduce open ranks for a given space */
 static inline void derank(u8 row, u8 col) {
   u8 dia = row + col;
-  u8 adg = N - col + row - 1;
+  u8 adg = antidiagonal(row, col);
   rk.rows[row].open--;
   rk.cols[col].open--;
   rk.dias[dia].open--;
   rk.adia[adg].open--;
-  rk.n_open--;
 }
 
 /* reduce open ranks for the current slot */
@@ -100,7 +107,6 @@ static inline void derank_sl() {
   rk.cols[sl.col].open--;
   rk.dias[sl.dia].open--;
   rk.adia[sl.adg].open--;
-  rk.n_open--;
 }
 
 /* increase placed ranks for the current slot */
@@ -113,12 +119,12 @@ static inline void placed_sl() {
 
 /* clear a full row */
 static inline void cf_row(u8 row) {
-  rk.open_rows &= ~((rk.rows[row].open == 0) << row); // is if() rk... faster?
+  rk.open_rows &= ~((rk.rows[row].open == 0) << row);
 }
 
 /* clear a full col */
 static inline void cf_col(u8 col) {
-  rk.open_cols &= ~((rk.cols[col].open == 0) << col); // is if() rk... faster?
+  rk.open_cols &= ~((rk.cols[col].open == 0) << col);
 }
 
 /* clear a full row and col */
@@ -127,25 +133,23 @@ static inline void clear_full(u8 row, u8 col) {
 }
 
 /* recompute ranks from scratch according to a given space */
-static inline void rerank(u16 row, u16 col) {
-  u16 diag = row + col;
-  u16 adia = N - col + row - 1;
-  if (at(row, col)) {
-    rk.rows[row].placed++;
-    rk.cols[col].placed++;
-    rk.dias[diag].placed++;
-    rk.adia[adia].placed++;
+static inline void rerank(Slot s) {
+  if (at(s.row, s.col)) {
+    rk.rows[s.row].placed++;
+    rk.cols[s.col].placed++;
+    rk.dias[s.dia].placed++;
+    rk.adia[s.adg].placed++;
   } else {
-    rk.rows[row].open++;
-    rk.cols[col].open++;
-    rk.dias[diag].open++;
-    rk.adia[adia].open++;
-    rk.n_open++;
-    rk.open_rows |= 1 << row;
-    rk.open_cols |= 1 << col;
+    rk.rows[s.row].open++;
+    rk.cols[s.col].open++;
+    rk.dias[s.dia].open++;
+    rk.adia[s.adg].open++;
+    rk.open_rows |= 1 << s.row;
+    rk.open_cols |= 1 << s.col;
   }
 }
 
+/* initialise boards and ranks */
 void init() {
   seed_rng();
   for (board = N; board--;) {
@@ -157,12 +161,12 @@ void init() {
   zero(rk);
   for (u8 i = 0; i < N; i++) {
     for (u8 j = 0; j < N; j++) {
-      rerank(i, j);
+      rerank(slot(i, j));
     }
   }
 
   assert(N);
-  assert(N <= 21);
+  assert(N <= 21); // going over requires threading & other work
   assert(sizeof(Rank));
   assert(sizeof(Ranks));
   assert(sizeof(Board));
