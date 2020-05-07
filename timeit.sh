1d0ffa9a300bae31c85496e5205bea3d47f57c26
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-05-07 15:27:57 +0100

    ballast table

--- timeit.sh
+++ timeit.sh
@@ -1,20 +1,20 @@
 echo " N | FIRSTROW" && echo "-----------------"
 
-for i in {9..13}
+for i in {9..14}
 do
   ./run.sh $i FIRSTROW $1
 done
 
 echo ""
 echo " N | SQUAREENUM" && echo "-----------------"
-for i in {9..13}
+for i in {9..14}
 do
   ./run.sh $i SQUAREENUM $1
 done
 
 echo ""
 echo " N | TAW" && echo "-----------------"
-for i in {9..13}
+for i in {9..14}
 do
   ./run.sh $i TAW $1
 done
